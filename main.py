59af5351b594bd411c021740583158988c667a0e
Satoshi Watanabe <sassy.watanabe@gmail.com>
2015-07-23 01:34:38 +0900

    add getting twitter_id list.

--- main.py
+++ main.py
@@ -6,11 +6,28 @@ import re
 import cookielib
 from bs4 import BeautifulSoup
 
-def main():
+def getIds():
+    url = 'http://www.mache.tv/m/twitter_pickup.php'
+    values = {"per_screen_name" : "mikichi0223" , "e" : "2016flash" }
+
+    data = urllib.urlencode(values)
+    f = urllib.urlopen(url + '?' + data)
+    soup = BeautifulSoup(f.read(), "html.parser")
+    a_elems = soup.find_all("a")
+    prog = re.compile("^https://twitter.com/mikichi0223/status/([0-9]+)", re.UNICODE)
+    ids = []
+    for a_elem in a_elems:
+        href = a_elem.get('href')
+        if href is not None and prog.match(href):
+            ids.append(prog.search(href).group(1))
+    return ids
+
+
+def vote(id):
     url = 'http://www.mache.tv/m/twitter_pickup/vote.php'
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
 
-    values = {"e_id" : "2016flash", "mode" : "vote_confirm", "p_id" : "14", "tweet_id" : "623637105617448960"}
+    values = {"e_id" : "2016flash", "mode" : "vote_confirm", "p_id" : "14", "tweet_id" : id}
     print(values)
     data = urllib.urlencode(values)
     print(data)
@@ -25,7 +42,7 @@ def main():
     vote_hash = p.search(info).group(1)
     print(vote_hash)
 
-    values2 = {"e_id" : "2016flash", "mode" : "vote_complete", "p_id" : "14", "tweet_id" : "623637105617448960", "vote_hash" : str(vote_hash)}
+    values2 = {"e_id" : "2016flash", "mode" : "vote_complete", "p_id" : "14", "tweet_id" : id, "vote_hash" : str(vote_hash)}
     print(values2)
 
     data2 = urllib.urlencode(values2)
@@ -39,6 +56,11 @@ def main():
         print(e.code)
     except URLError, e:
         print(e.code)
+
+def main():
+    ids = getIds()
+    for id in ids:
+        vote(id)
 
 
 if __name__ == '__main__':
